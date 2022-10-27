d408030c958318b1f8164faa6a97db27f943e988
Dar-Na <70232815+Dar-Na@users.noreply.github.com>
2022-10-27 16:25:53 +0200

    created airplanes generator

--- deps.py
+++ deps.py
@@ -1,6 +1,7 @@
 import datetime
 import random
 
+LETTERS = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 def parse(file):
     temp = open(file, "r")
