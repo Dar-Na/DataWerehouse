969da8aad7625aa9ea4a9aa20c94a80158c91365
Dar-Na <70232815+Dar-Na@users.noreply.github.com>
2022-11-03 14:32:13 +0100

    changed names of files

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     end_date_one = datetime.date(2020, 12, 31)
 
     # NUMBER OF PESELS
-    num = 1500
+    num = 2000
     # GENERATE PESELS
     PESELS = pilot.generatePESELs(num=num, start_date=start_date_one, end_date=end_date_one)
     print(PESELS)
@@ -35,37 +35,37 @@ if __name__ == "__main__":
     # GENERATE PILOTS
     pilots = pilot.generatePilots(PESELs=PESELS, names=names, surnames=surnames)
     print(pilots)
-    save(pilots, "T0-T1/pilots.csv")
+    save(pilots, "T0-T1/PILOTS.csv")
     # GENERATE AIRPLANES
-    samolots = samolot.generateSamolts(num=100)
+    samolots = samolot.generateSamolts(num=1000)
     print(samolots)
-    save(samolots, "T0-T1/samolots.csv")
+    save(samolots, "T0-T1/SAMOLOTS.csv")
     # GENERATE KURSES
     kurses = kurs.generateKurses(num=10)
     kurses = deps.delDuplicates(kurses)
     print(kurses)
-    save(kurses, "T0-T1/kurses.csv")
+    save(kurses, "T0-T1/KURSES.csv")
 
     # CALCULATE TIME FROM ONE CITY TO ANOTHER
     times = deps.time(kurses)
     print(times)
-    save(times, "T0-T1/times.csv")
+    save(times, "T0-T1/TIMES.csv")
 
     # GENERATE LOTY, AWARII AND PILOTÓW
     loty1, awarii, pilociWLocie = loty.generateLoty(
-        num=6000, kurses=kurses, samolots=samolots, pilotes=pilots,
+        num=60000, kurses=kurses, samolots=samolots, pilotes=pilots,
         times=times, start_date=start_date_one, end_date=end_date_one, kontrolerzy=kontrolerzy
     )
-    save(loty1, "T0-T1/loty.csv")
-    save(awarii, "T0-T1/awarii.csv")
-    save(pilociWLocie, "T0-T1/pilociWLocie.csv")
+    save(loty1, "T0-T1/LOTY.csv")
+    save(awarii, "T0-T1/AWARII.csv")
+    save(pilociWLocie, "T0-T1/PILOCIWLOCIE.csv")
 
     # OKRES Z T1 DO T2
     start_date_two = datetime.date(2021, 1, 1)
     end_date_two = datetime.date(2022, 12, 31)
 
     # NUMBER OF PESELS
-    num = 4000
+    num = 10000
     # GENERATE PESELS
     PESELS2 = pilot.generatePESELs(num=num, start_date=start_date_two, end_date=end_date_two)
     print(PESELS2)
@@ -82,20 +82,20 @@ if __name__ == "__main__":
     save(newName, "newName.csv")
 
     # GENERATE AIRPLANES
-    samolots2 = samolot.generateSamolts(num=150)
+    samolots2 = samolot.generateSamolts(num=1500)
     print(samolots2)
     # MERGE TABLES AIRPLANES
     SAMOLOTS = samolots + samolots2
-    save(SAMOLOTS, "T0-T2/samolots.csv")
+    save(SAMOLOTS, "T0-T2/SAMOLOTS.csv")
 
     # GENERATE KURSES
-    kurses2 = kurs.generateKurses(num=20)
+    kurses2 = kurs.generateKurses(num=30)
     kurses2 = deps.delDuplicates(kurses2)
     print(kurses2)
     # MERGE TABLES KURSES
     KURSES = kurses + kurses2
     KURSES = KURSES
-    save(KURSES, "T0-T2/kurses.csv")
+    save(KURSES, "T0-T2/KURSES.csv")
 
     # CALCULATE TIME FROM ONE CITY TO ANOTHER
     times2 = deps.time(kurses2)
@@ -103,14 +103,14 @@ if __name__ == "__main__":
     # MERGE TABLES TIME
     TIMES = times + times2
     # TIMES = list(dict.fromkeys(TIMES))
-    save(TIMES, "T0-T2/times.csv")
+    save(TIMES, "T0-T2/TIMES.csv")
 
     # GENERATE LOTY, AWARII AND PILOTÓW
     loty2, awarii2, pilociWLocie2 = loty.generateLoty(
-        num=10000, kurses=KURSES, samolots=SAMOLOTS, pilotes=PILOTS,
+        num=800000, kurses=KURSES, samolots=SAMOLOTS, pilotes=PILOTS,
         times=TIMES, start_date=start_date_one, end_date=end_date_one, kontrolerzy=kontrolerzy
     )
 
-    save(loty1 + loty2, "T0-T2/loty.csv")
-    save(awarii + awarii2, "T0-T2/awarii.csv")
-    save(pilociWLocie + pilociWLocie2, "T0-T2/pilociWLocie.csv")
+    save(loty1 + loty2, "T0-T2/LOTY.csv")
+    save(awarii + awarii2, "T0-T2/AWARII.csv")
+    save(pilociWLocie + pilociWLocie2, "T0-T2/PILOCIWLOCIE.csv")
